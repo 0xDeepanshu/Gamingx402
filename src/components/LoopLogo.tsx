0ab5e85ccfde6fd2572bcc4f686330583e9a5d42
0xDeepanshu <wtfdeep@duck.com>
2025-11-02 01:58:16 +0530

    fixed link

--- src/components/LoopLogo.tsx
+++ src/components/LoopLogo.tsx
@@ -4,13 +4,13 @@ import LogoLoop from './ui/LogoLoop';
 // Alternative with image sources
 const imageLogos = [
   { src: "/marquee/thirdweb.svg", alt: "Company 1",  },
-  { src: "/marquee/Base_lockup_white.svg", alt: "Company 3", href: "https://company3.com" },
-  { src: "/marquee/unreal4.svg", alt: "Company 3", href: "https://company3.com" },
-  { src: "/marquee/unity.svg", alt: "Company 3", href: "https://company3.com" },
-  { src: "/marquee/solana.svg", alt: "Company 2", href: "https://company2.com" },
-  { src: "/marquee/farcaster.svg", alt: "Company 3", href: "https://company3.com" },
-  { src: "/marquee/ethereum-eth-logo.svg", alt: "Company 3", href: "https://company3.com" },
-   { src: "/marquee/arbitrum.svg", alt: "Company 3", href: "https://company3.com" },
+  { src: "/marquee/Base_lockup_white.svg", alt: "Company 3"},
+  { src: "/marquee/unreal4.svg", alt: "Company 3"},
+  { src: "/marquee/unity.svg", alt: "Company 3"},
+  { src: "/marquee/solana.svg", alt: "Company 2" },
+  { src: "/marquee/farcaster.svg", alt: "Company 3"},
+  { src: "/marquee/ethereum-eth-logo.svg", alt: "Company 3"},
+   { src: "/marquee/arbitrum.svg", alt: "Company 3"},
 ];
 
 export default function Loop() {
